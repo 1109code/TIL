3bd275507083dd9a9c671f0a2384f57eae816a6b
1109code <1109ssh.code@gmail.com>
2022-09-20 02:34:14 +0900

    great

--- 알고리즘/BOJ/BOJ_11725.py
+++ 알고리즘/BOJ/BOJ_11725.py
@@ -1,19 +1,26 @@
 # BOJ_11725 트리의 부모 찾기
 # 2022-09-16
-def dfs(n):
-    global next
-    while tree[n]:
-        next.append(tree[n].pop())
-        par[next] = n
-        dfs(a)
+import sys
+sys.setrecursionlimit(10**6)
 
+def dfs(s):
+    while tree[s]:
+        p = tree[s].pop()
+        tree[p].remove(s)
+        par[p] = s
+        dfs(p)
 
 
 N = int(input())
+
 tree = [[] for _ in range(N + 1)]
 par = [0 for _ in range(N + 1)]
-next = []
-for n in range(N):
+
+for n in range(N-1):
     a, b = map(int, input().split())
     tree[a].append(b)
     tree[b].append(a)
+
+dfs(1)
+for i in range(2, N+1):
+    print(par[i])
