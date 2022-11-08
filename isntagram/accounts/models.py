31b288eeaa03ee236ec530dd63fb341e4a61163a
unknown <1109ssh@naver.com>
2022-11-09 02:57:26 +0900

    great

--- isntagram/accounts/models.py
+++ isntagram/accounts/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=40)
+    introduction = models.CharField(blank=True)
+    image_file = models.ImageField(upload_to='', height_field='300', width_field='300', quality=90, format='JPEG')
