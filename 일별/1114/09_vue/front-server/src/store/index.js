47a4fcba764cd2a26e53d63be07bd4b3cd888d98
YoungChanRho <0chan7@hanmail.net>
2022-11-14 17:49:33 +0900

    great

---
+++ 일별/1114/09_vue/front-server/src/store/index.js
@@ -0,0 +1,89 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import createdPersistedState from 'vuex-persistedstate'
+import router from '@/router'
+
+Vue.use(Vuex)
+
+const API_URL = 'http://127.0.0.1:8000'
+
+
+export default new Vuex.Store({
+  plugins: [
+    createdPersistedState()
+  ],
+  state: {
+    articles: [],
+    token: null,
+  },
+  getters: {
+    isLogin(state) {
+      return state.token ? true : false
+    }
+  },
+  mutations: {
+    GET_ARTICLES(state, articles) {
+      state.articles = articles
+    },
+    // 회원가입 && 로그인
+    SAVE_TOKEN(state, token) {
+      state.token = token
+      router.push({ name: 'ArticleView' })
+    }
+  },
+  actions: {
+    getArticles(context) {
+      axios({
+        method: 'get',
+        url: `${API_URL}/api/v1/articles/`
+      })
+        .then((res) => {
+          // console.log(res, context)
+          // console.log(res.data)
+          context.commit('GET_ARTICLES', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    signUp(context, payload) {
+      const username = payload.username
+      const password1 = payload.password1
+      const password2 = payload.password2
+
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/signup/`,
+        data: {
+          username,
+          password1,
+          password2,
+          // username: payload.username,
+          // password1: payload.password1,
+          // password2: payload.password2,
+        }
+      })
+        .then(res => {
+          // console.log(res)
+          context.commit('SAVE_TOKEN', res.data.key)
+        })
+    },
+    logIn(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/login/`,
+        data: {
+          username: payload.username,
+          password: payload.password,
+        }
+      })
+        .then((res) => {
+          console.log(res)
+          context.commit('SAVE_TOKEN', res.data.key)
+        })
+    }
+  },
+  modules: {
+  }
+})
