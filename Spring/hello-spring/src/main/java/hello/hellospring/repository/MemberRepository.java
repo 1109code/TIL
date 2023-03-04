6211fb2604b2c5728749d163bae7759478c73f5d
1109code <1109ssh.code@gmail.com>
2023-03-04 17:31:03 +0900

    pjt

--- Spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Repository // spring이 repository 임을 인식
 public interface MemberRepository {
     Member save(Member member);
     // Optional : null 처리
